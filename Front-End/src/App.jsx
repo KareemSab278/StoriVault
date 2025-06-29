5fa45f1f3841764fc59de179ab963724c81d0a55
Kareem Elsabrouty <10288694@student.walsallcollege.ac.uk>
2025-06-29 22:26:35 +0100

    Added animation tags
    added animation tags in app.js for frontend team to simplify development process and reduce possibility for bugs

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { motion } from 'framer-motion' // added in for simple and quick animations. no need for writing any yourself. saves hastle.
 import './App.css'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+      {/* fade in */}
+      <motion.div initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <h1>Welcome to StoriVault!</h1>
+        <p>fade in text example. This doesnt apply to just text. it applies to ANY component added under the tags</p>
+      </motion.div>
+
+      {/* notice the motion.div tags, you can set up how each element in the tag appears.
+      i added this for better animations and ease of use. saves time for frontend team
+      just simply put everything in one tag and thats it. */}
+
     </>
   )
 }
